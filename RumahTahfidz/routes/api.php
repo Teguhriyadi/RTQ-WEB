67d2e84671ba3075e162affcaf0e8f46ec3b1f65
Teguhriyadi <ilham.teguh55@gmail.com>
2022-03-04 12:07:38 +0700

    Membuat URL Login

--- RumahTahfidz/routes/api.php
+++ RumahTahfidz/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// Authentication
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('login_admin', 'loginAdmin');
+    Route::post('register', 'register');
 });
