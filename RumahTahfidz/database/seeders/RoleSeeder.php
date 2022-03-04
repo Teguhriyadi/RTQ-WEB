8393acbf3961322a72c3565dde7edce8b658eade
Teguhriyadi <ilham.teguh55@gmail.com>
2022-03-05 06:55:45 +0700

    Menambahkan Seeder Role

--- RumahTahfidz/database/seeders/RoleSeeder.php
+++ RumahTahfidz/database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,12 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Role::create([
+            "keterangan" => "Admin"
+        ]);
+
+        Role::create([
+            "keterangan" => "Pengajar"
+        ]);
     }
 }
