d403f2ef157157cb940f1c6d88d1a1ff9d60951b
Teguhriyadi <ilham.teguh55@gmail.com>
2022-03-04 12:21:19 +0700

    Push Login

--- RumahTahfidz/app/Http/Controllers/API/AuthController.php
+++ RumahTahfidz/app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
