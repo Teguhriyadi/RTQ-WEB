255e8a7f9a4d4bc82a188c9b902783311f346ff8
hakim-asrori <saneglos005@gmail.com>
2022-03-04 17:06:40 +0700

    perbaharui api

--- RumahTahfidz/routes/api.php
+++ RumahTahfidz/routes/api.php
@@ -1,7 +1,16 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\RoleController;
+use App\Http\Controllers\API\SiswaController;
+use App\Http\Controllers\API\PengajarController;
+use App\Http\Controllers\API\AbsensiController;
+use App\Http\Controllers\API\UsersController;
+use App\Http\Controllers\API\StatusAbsenController;
+use App\Http\Controllers\ProfilController;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +24,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route Percobaan
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    return Auth::user();
+});
+
+Route::get('users', function () {
+    $user = User::all();
+
+    return response()->json([
+        'message' => 'Response Successful',
+        'data' => $user
+    ]);
 });
 
 // Authentication
@@ -25,3 +44,26 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('login_admin', 'loginAdmin');
     Route::post('register', 'register');
 });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
+
+// Role
+Route::resource('role', RoleController::class);
+
+// Siswa
+Route::resource('siswa', SiswaController::class);
+
+// Pengajar
+Route::resource('pengajar', PengajarController::class);
+
+// Absensi
+Route::resource('absensi', AbsensiController::class);
+
+// Status Absen
+Route::resource('status_absen', StatusAbsenController::class);
+
+Route::resource('users', UsersController::class);
+
+Route::get('info_profil/{no_hp}', [ProfilController::class, 'info_profil']);
