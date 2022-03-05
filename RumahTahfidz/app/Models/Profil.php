a5967c13fd9d13b9087db97bf1eb992d9ad02569
Teguhriyadi <ilham.teguh55@gmail.com>
2022-03-05 13:56:00 +0700

    Membuat Migration & Model

---
+++ RumahTahfidz/app/Models/Profil.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profil extends Model
+{
+    use HasFactory;
+
+    protected $table = "tb_profil";
+
+    protected $guarded = [''];
+
+}
---
+++ RumahTahfidz/database/migrations/2022_03_05_135132_create_tb_profil_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_profil', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama", 50);
+            $table->string("singkatan", 20);
+            $table->string("no_hp", 20);
+            $table->string("email", 100);
+            $table->text("alamat");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_profil');
+    }
+};
