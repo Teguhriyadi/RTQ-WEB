e777cef5396fd5a5aeb3f2132c277f3b005df85b
Teguhriyadi <ilham.teguh55@gmail.com>
2022-03-07 21:12:54 +0700

    Mengedit Profil WEB Pada Landing Page

--- RumahTahfidz/resources/views/app/landing/layouts/partials/footer/v_footer.blade.php
+++ RumahTahfidz/resources/views/app/landing/layouts/partials/footer/v_footer.blade.php
@@ -3,10 +3,27 @@
     <div class="container py-5">
         <div class="row g-5">
             <div class="col-lg-3 col-md-6">
-                <h4 class="text-light mb-4">Address</h4>
-                <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>+012 345 67890</p>
-                <p class="mb-2"><i class="fa fa-envelope me-3"></i>info@example.com</p>
+                <h4 class="text-light mb-4">Alamat</h4>
+                <?php
+                    use App\Models\Profil;
+
+                    $data = Profil::select("alamat", "no_hp", "email")->first();
+                ?>
+                    <p class="mb-2">
+                        <i class="fa fa-map-marker-alt me-3"></i>
+                        {{ $data->alamat }}
+                    </p>
+                    <p class="mb-2">
+                        <i class="fa fa-phone-alt me-3"></i>
+                        {{ $data->no_hp }}
+                    </p>
+                    <p class="mb-2">
+                        <i class="fa fa-envelope me-3"></i>
+                        {{ $data->email }}
+                    </p>
+                <?php
+
+                ?>
                 <div class="d-flex pt-2">
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
