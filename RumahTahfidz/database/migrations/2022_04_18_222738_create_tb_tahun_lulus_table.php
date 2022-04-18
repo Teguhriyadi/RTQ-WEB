2905285a85b76f4a15ccb46e8b3902fa0213247e
Teguhriyadi <ilham.teguh55@gmail.com>
2022-04-18 22:33:16 +0700

    Cancel

---
+++ RumahTahfidz/database/migrations/2022_04_18_222738_create_tb_tahun_lulus_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_tahun_lulus', function (Blueprint $table) {
+            $table->id();
+            $table->year("tahun_lulus");
+            $table->enum("status", [1, 0])->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_tahun_lulus');
+    }
+};
