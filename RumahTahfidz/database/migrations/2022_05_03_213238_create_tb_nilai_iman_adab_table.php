0971048d393ebdb968dc20fc8ed02246055bf7fa
Teguhriyadi <ilham.teguh55@gmail.com>
2022-05-03 21:38:54 +0700

    Push

---
+++ RumahTahfidz/database/migrations/2022_05_03_213238_create_tb_nilai_iman_adab_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_nilai_iman_adab', function (Blueprint $table) {
+            $table->id();
+            $table->integer("id_asatidz");
+            $table->integer("id_santri");
+            $table->integer("id_pelajaran_iman_adab");
+            $table->double("nilai");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_nilai_iman_adab');
+    }
+};
