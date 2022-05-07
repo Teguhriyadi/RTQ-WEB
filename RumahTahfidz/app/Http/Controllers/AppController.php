de7e896a0ff1eb87a6197b25295852586ea2801d
Teguhriyadi <ilham.teguh55@gmail.com>
2022-05-07 23:33:11 +0700

    Merapihkan Halaman Dashboard, Membuat Tampilan Grafik

--- RumahTahfidz/app/Http/Controllers/AppController.php
+++ RumahTahfidz/app/Http/Controllers/AppController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Asatidz;
+use App\Models\Santri;
+use App\Models\AdminLokasiRt;
+
 use App\Models\TerakhirLogin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -16,6 +21,10 @@ class AppController extends Controller
     public function home()
     {
         $data = [
+            "jumlah_user" => User::count(),
+            "jumlah_asatidz" => Asatidz::count(),
+            "jumlah_santri" => Santri::count(),
+            "jumlah_admin_lokasi_rt" => AdminLokasiRT::count(),
             "user_login" => TerakhirLogin::where("id_user", auth()->user()->id)->get()
         ];
 
