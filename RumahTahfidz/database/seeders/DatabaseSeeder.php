72586111cf7ecb88f5e46d2e87426e9004e9a526
Teguhriyadi <ilham.teguh55@gmail.com>
2022-06-02 14:12:48 +0700

    PUSH

--- RumahTahfidz/database/seeders/DatabaseSeeder.php
+++ RumahTahfidz/database/seeders/DatabaseSeeder.php
@@ -35,5 +35,6 @@ class DatabaseSeeder extends Seeder
         $this->call(KelasHalaqahSeeder::class);
         $this->call(NilaiKategoriSeeder::class);
         $this->call(HakAksesSeeder::class);
+        $this->call(HafalanAsatidzSeeder::class);
     }
 }
