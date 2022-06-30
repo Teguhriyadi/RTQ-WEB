4a86571b555ef5c3433a63361f40eff00d359929
Teguhriyadi <ilham.teguh55@gmail.com>
2022-06-30 09:53:22 +0700

    PUSH

--- RumahTahfidz/database/seeders/StatusAbsenSeeder.php
+++ RumahTahfidz/database/seeders/StatusAbsenSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\StatusAbsen;
 
 class StatusAbsenSeeder extends Seeder
 {
