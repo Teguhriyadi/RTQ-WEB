619936a723613e5c9f3638508c33e74a21e0e24c
Teguhriyadi <ilham.teguh55@gmail.com>
2022-07-19 11:57:12 +0700

    PUSH

--- RumahTahfidz/resources/views/app/layouts/partials/sidebar/main-sidebar.blade.php
+++ RumahTahfidz/resources/views/app/layouts/partials/sidebar/main-sidebar.blade.php
@@ -11,7 +11,7 @@ $profil = \App\Models\ProfilWeb::select('nama')->first();
                 <i class="fa fa-paw"></i>
                 <span>
                     @if (empty($profil))
-                        RTQ
+                        RT
                     @else
                         @if (strlen($profil->nama) > 15)
                             RTQ ...
