1813b289daf46675fc9e2e9968469115caab0831
Hakim Asrori <77718626+hakim-asrori@users.noreply.github.com>
2023-01-24 15:16:46 +0700

    Update v_login.blade.php

--- RumahTahfidz/resources/views/app/auth/v_login.blade.php
+++ RumahTahfidz/resources/views/app/auth/v_login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login | RTQ Ulil Albab</title>
+    <title>Login | RTQ Ulil Albab 2</title>
     <link rel="icon" type="image/png" href="{{ url('gambar/logo_ulil.png') }}" />
 
     <!-- Fonts -->
